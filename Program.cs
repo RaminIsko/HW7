f915ed9e7edf11701106ba6e1f90e0bdb62f776e
RaminIsko <ramsmagast@gmail.com>
2023-03-05 16:07:56 +0600

    Задача №53

--- Program.cs
+++ Program.cs
@@ -2,20 +2,26 @@
 using static System.Console;
 
 
-double[,] GetDoubleArray (int rows, int columns, int minValue, int maxValue){
+double[,] GetDoubleArray(int rows, int columns, int minValue, int maxValue)
+{
     double[,] result = new double[rows, columns];
-    for(int i = 0; i < rows; i++){
-        for(int j = 0; j < columns; j++){
-            result[i,j] = Convert.ToDouble(new Random().Next(minValue * 10, (maxValue + 1) * 10) / 10.0);;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = Convert.ToDouble(new Random().Next(minValue * 10, (maxValue + 1) * 10) / 10.0); ;
         }
     }
     return result;
 }
 
-void PrintDoubleArray (double[,] array) {
-    for(int i = 0; i < array.GetLength(0); i++){
-        for(int j = 0; j < array.GetLength(1); j++){
-            Write($"{array[i,j]} ");
+void PrintDoubleArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"{array[i, j]} ");
         }
         WriteLine();
     }
@@ -40,47 +46,60 @@ PrintDoubleArray(array);
 
 
 
-int InputRowIndex (){ 
+int InputRowIndex()
+{
     Write("Введите индекс строки: ");
     int rowIndex = int.Parse(ReadLine()!);
     return rowIndex;
 }
 
-int InputColumnIndex (){
+int InputColumnIndex()
+{
     Write("Введите индекс столбца: ");
     int columnIndex = int.Parse(ReadLine()!);
     return columnIndex;
 }
 
-int[,] GetArray (int rows, int columns, int minValue, int maxValue){
+int[,] GetArray(int rows, int columns, int minValue, int maxValue)
+{
     int[,] array = new int[rows, columns];
-    for(int i = 0; i < rows; i++){
-        for(int j = 0; j < columns; j++){
-            array[i,j] = new Random().Next(10,20);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(10, 20);
         }
     }
     return array;
 }
 
-void PrintArray (int[,] array){
-    for(int i = 0; i < array.GetLength(0); i++){
-        for(int j = 0; j < array.GetLength(1); j++){
-            Write($"{array[i,j]} ");
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"{array[i, j]} ");
         }
         WriteLine();
     }
 }
 
-void isNumberEqualIndex (int[,] table ,int[] array, int rows, int columns) {
-    for(int i = 0; i < table.GetLength(0); i++){
-        for(int j = 0; j < table.GetLength(1); j++){
-            if(array[0] == i && array[1] == j){
-                WriteLine($"Число в таблице с индексами {array[0]}, {array[1]} - {table[i,j]}");
+void isNumberEqualIndex(int[,] table, int[] array, int rows, int columns)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            if (array[0] == i && array[1] == j)
+            {
+                WriteLine($"Число в таблице с индексами {array[0]}, {array[1]} - {table[i, j]}");
             }
         }
     }
-    if((array[0] >= rows && array[1] >= columns) || (array[0] < 0 && array[1] < 0)){
-                WriteLine($"Числа с индексами {array[0]}, {array[1]} - Нет");
+    if ((array[0] >= rows && array[1] >= columns) || (array[0] < 0 && array[1] < 0))
+    {
+        WriteLine($"Числа с индексами {array[0]}, {array[1]} - Нет");
     }
 }
 
@@ -98,11 +117,46 @@ int minValue = int.Parse(ReadLine()!);
 Write("Введите максимальное значение элементов: ");
 int maxValue = int.Parse(ReadLine()!);
 
-int[] rowColumnIndex = {InputRowIndex(), InputColumnIndex()};
+int[] rowColumnIndex = { InputRowIndex(), InputColumnIndex() };
 
 int[,] array2 = GetArray(rows2, columns2, minValue, maxValue);
 PrintArray(array2);
 isNumberEqualIndex(array2, rowColumnIndex, rows2, columns2);
+
+
+
+Clear();
+
+void ColumnAverageArifmetic(int[,] table)
+{
+    Write("Среднее арифметическое каждого столбца: ");
+    for (int i = 0; i < table.GetLength(0); i++){
+        double array2 = 0;
+        for (int j = 0; j < table.GetLength(1); j++){
+            array2 += Convert.ToDouble(array[i, j]);
+        }
+        Write($"{array2 / table.GetLength(0)}; ");
+    }
+}
+
+WriteLine("Задача №53: ");
+
+Write("Введите количество строк в массиве: ");
+int rows3 = int.Parse(ReadLine()!);
+
+Write("Введите количество столбцов в массиве: ");
+int columns3 = int.Parse(ReadLine()!);
+
+Write("Введите минимальное значение элементов: ");
+int minValue3 = int.Parse(ReadLine()!);
+
+Write("Введите максимальное значение элементов: ");
+int maxValue3 = int.Parse(ReadLine()!);
+
+int[,] array3 = GetArray(rows3, columns3, minValue3, maxValue3);
+PrintArray(array3);
+ColumnAverageArifmetic(array3);
+
 
 
 
